abdeed89f3e012f99013d09781429255a2047c00
David <davidcai918@gmail.com>
2022-10-28 01:49:54 +0100

    add memory for users who leave and rejoin

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-from ast import Raise
 import re
 import os
 import os.path as path
+from xmlrpc.client import Boolean
 from icu_ea_api import ICUEActivitiesAPI
 import sqlite3 as sq
+from dotenv import load_dotenv
+load_dotenv()
 
 '''init union API'''
 csp_code = 625
@@ -15,10 +17,11 @@ society_api = ICUEActivitiesAPI(csp_code, api_key, year)
 INIT_SCHEMA = '''
 CREATE TABLE mapping (
     user_id  TEXT    PRIMARY KEY,
-    shortcode   TEXT    NOT NULL
+    shortcode   TEXT    NOT NULL,
+    active  INTEGER DEFAULT 1
 )
 '''
-DB_PATH = path.abspath(os.getcwd()) + r'\\memberMap.db'
+DB_PATH = os.getenv('DB_PATH')
 SHORTCODE_REGEX = r'[a-z]{2,3}[0-9]{2,4}'
 
 
@@ -50,8 +53,8 @@ def add_mapping(shortcode, userid, db=DB_PATH):
     if is_shortcode(shortcode):
         cur.execute('''
             INSERT INTO mapping 
-            VALUES (?,?)
-        ''', (userid.lower().strip(), shortcode.lower().strip())
+            VALUES (?,?,?)
+        ''', (userid.lower().strip(), shortcode.lower().strip(), 1)
         )
         conn.commit()
     else:
@@ -68,3 +71,34 @@ def shortcode_exists(shortcode, db=DB_PATH):
     else:
         raise Exception('Invalid shortcode')
 
+def valid_mapping(shortcode, userid, db=DB_PATH):
+    conn = sq.connect(db)
+    cur = conn.cursor()
+    if is_shortcode(shortcode):
+        cur.execute('''
+        SELECT active FROM mapping WHERE shortcode = ? AND user_id = ?
+        ''', (shortcode.lower().strip(),str(userid))
+        )
+        val = cur.fetchall()#
+        if any(val):
+            valid = [0][0]
+        else:
+            valid = 1
+        print(valid)
+        return bool(valid)
+    else:
+        raise Exception('Invalid shortcode')
+
+def change_valid(userid, valid:int, db=DB_PATH):
+    conn = sq.connect(db)
+    cur = conn.cursor()
+    if (valid in {0,1}) :
+        cur.execute('''
+        UPDATE mapping 
+        SET active = ? 
+        WHERE user_id = ?
+        ''', (valid,str(userid))
+        )
+        conn.commit()
+    else:
+        raise Exception('Issue changing valid status')
