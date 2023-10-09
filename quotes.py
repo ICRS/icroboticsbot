76d69aaf2a8f78ae829896a824d677f318b2eb09
pi <pi@icrs>
2023-10-09 19:45:05 +0100

    updated bot

--- quotes.py
+++ quotes.py
@@ -1,9 +1,8 @@
 from Quote2Image import convert
 from PIL import Image
 
-IMAGE_PATH = "background_images/PC_1.jpg"
 # Font Size Default to 32, Height and Width by default is 612
-def generate(IMAGE_PATH,author, quote,IMAGE_TEMP="temp.jpg", font="fonts/Precious.ttf"):
+def generate(IMAGE_PATH,author, quote,IMAGE_TEMP="/home/pi/code/icroboticsbot/temp.jpg", font="/home/pi/code/icroboticsbot/fonts/Precious.ttf"):
 	image = Image.open(IMAGE_PATH)
 	grayscale = image.convert("L")
 	width, height = grayscale.size
@@ -17,10 +16,10 @@ def generate(IMAGE_PATH,author, quote,IMAGE_TEMP="temp.jpg", font="fonts/Preciou
 	fg="white",
 	image=IMAGE_TEMP,
 	border_color="black",
-	font_size=50,
+	font_size=40,
 	font_file=font,
 	width=400,
 	height=400)
 
 # Save The Image as a Png file
-	img.save("quote.png")
+	img.save("/home/pi/code/icroboticsbot/quote.png")
