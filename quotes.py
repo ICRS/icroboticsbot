ff13b308698b48c5ab8fd5721c7f7b2cf416a895
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-12-10 05:05:14 +0000

    Fixed formatting

--- quotes.py
+++ quotes.py
@@ -1,25 +1,47 @@
+"""
+Generate a Quote Image from a given Image and Quote
+"""
+
+from PIL import Image  # type: ignore
+
 from Quote2Image import convert
-from PIL import Image
+
 
 # Font Size Default to 32, Height and Width by default is 612
-def generate(IMAGE_PATH,author, quote,IMAGE_TEMP="/home/pi/code/icroboticsbot/temp.jpg", font="/home/pi/code/icroboticsbot/fonts/Precious.ttf"):
+def generate(IMAGE_PATH, author, quote,
+			 IMAGE_TEMP="/home/pi/code/icroboticsbot/temp.jpg", # noqa
+			 font="/home/pi/code/icroboticsbot/fonts/Precious.ttf"): # noqa
+	"""
+	generate a quote image from a given image and quote
+
+	Parameters
+	----------
+	IMAGE_PATH : String
+		Path to the Image
+	author : String
+		Author of quote
+	quote : String
+		Quote
+	IMAGE_TEMP : str, optional
+		Path to temp image, by default "/home/pi/code/icroboticsbot/temp.jpg"
+	"""
 	image = Image.open(IMAGE_PATH)
 	grayscale = image.convert("L")
 	width, height = grayscale.size
 	ratio = 400/width
-	grayscale = grayscale.resize((int(width*ratio),int(height*ratio)))
+	grayscale = grayscale.resize((int(width*ratio), int(height*ratio)))
 	grayscale.save(IMAGE_TEMP)
 
-	img=convert(
-	quote=quote,
-	author=author,
-	fg="white",
-	image=IMAGE_TEMP,
-	border_color="black",
-	font_size=40,
-	font_file=font,
-	width=400,
-	height=400)
+	img = convert(
+		quote=quote,
+		author=author,
+		fg="white",
+		image=IMAGE_TEMP,
+		border_color="black",
+		font_size=40,
+		font_file=font,
+		width=400,
+		height=400)
 
-# Save The Image as a Png file
+	# Save The Image as a Png file
 	img.save("/home/pi/code/icroboticsbot/quote.png")
