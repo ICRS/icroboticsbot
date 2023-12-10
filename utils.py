589391a191e7dd502e6bf678fb7afde9d8ca1adf
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-12-10 04:50:51 +0000

    Updated Utils docs

--- utils.py
+++ utils.py
@@ -14,10 +14,10 @@ from scp import SCPClient
 import requests
 import io
 
-TARGET_PATH = "/home/member/Downloads/"
+TARGET_PATH = '/home/member/Downloads/'
 load_dotenv()
 
-"""init union API"""
+'''init union API'''
 date_now = date.today()
 month_now = date_now.month
 year_now = str(date_now.year)
@@ -26,34 +26,34 @@ if month_now > 8:
 else:
     year_string = f"{int(year_now[2:])-1}-{year_now[2:]}"
 csp_code = 625
-api_key = os.getenv("API_KEY")
-file_path = os.getenv("FILE_PATH")
+api_key = os.getenv('API_KEY')
+file_path = os.getenv('FILE_PATH')
 society_api = ICUEActivitiesAPI(csp_code, api_key, year_string)
-slicer_secret = os.getenv("SLICER_PW")
+slicer_secret = os.getenv('SLICER_PW')
 
-extension_list = ["stl", "3mf", "obj", "stp", "step"]
+extension_list = ['stl','3mf','obj','stp','step']
 
-INIT_SCHEMA = """
+INIT_SCHEMA = '''
 CREATE TABLE mapping (
     user_id  TEXT    PRIMARY KEY,
     shortcode   TEXT    NOT NULL,
     active  INTEGER DEFAULT 1
 )
-"""
-DB_PATH = os.getenv("DB_PATH")
-SHORTCODE_REGEX = r"[a-z]{2,3}[0-9]{2,4}"
+'''
+DB_PATH = os.getenv('DB_PATH')
+SHORTCODE_REGEX = r'[a-z]{2,3}[0-9]{2,4}'
 
 
 def is_shortcode(message: str):
-    """returns if a given string contains a shortcode"""
+    '''returns if a given string contains a shortcode'''
     message = message.lower()
     found = re.findall(SHORTCODE_REGEX, message)
     return any(found)
 
 
 def is_member(shortcode: str):
-    """returns if a given shortcode belongs to a member"""
-    return shortcode in [member["Login"] for member in society_api.list_members()]
+    '''returns if a given shortcode belongs to a member'''
+    return shortcode in [member['Login'] for member in society_api.list_members()]
 
 
 def init_db(db=DB_PATH):
@@ -66,48 +66,39 @@ def init_db(db=DB_PATH):
 
 
 def add_mapping(shortcode, userid, db=DB_PATH):
-    userid = str(userid)
+    userid=str(userid)
     conn = sq.connect(db)
     cur = conn.cursor()
     if is_shortcode(shortcode):
-        cur.execute(
-            """
+        cur.execute('''
             INSERT INTO mapping 
             VALUES (?,?,?)
-        """,
-            (userid.lower().strip(), shortcode.lower().strip(), 1),
+        ''', (userid.lower().strip(), shortcode.lower().strip(), 1)
         )
         conn.commit()
     else:
-        raise Exception("Invalid shortcode")
-
+        raise Exception('Invalid shortcode')
 
 def shortcode_exists(shortcode, db=DB_PATH):
     conn = sq.connect(db)
     cur = conn.cursor()
     if is_shortcode(shortcode):
-        cur.execute(
-            """
+        cur.execute('''
         SELECT * FROM mapping WHERE shortcode = ?
-        """,
-            (shortcode.lower().strip(),),
-        )
+        ''', (shortcode.lower().strip(),))
         return any(cur.fetchall())
     else:
-        raise Exception("Invalid shortcode")
-
+        raise Exception('Invalid shortcode')
 
 def valid_mapping(shortcode, userid, db=DB_PATH):
     conn = sq.connect(db)
     cur = conn.cursor()
     if is_shortcode(shortcode):
-        cur.execute(
-            """
+        cur.execute('''
         SELECT active FROM mapping WHERE shortcode = ? AND user_id = ?
-        """,
-            (shortcode.lower().strip(), str(userid)),
+        ''', (shortcode.lower().strip(),str(userid))
         )
-        val = cur.fetchall()  #
+        val = cur.fetchall()#
         if any(val):
             valid = val[0][0]
         else:
@@ -115,58 +106,46 @@ def valid_mapping(shortcode, userid, db=DB_PATH):
         print(valid)
         return bool(valid)
     else:
-        raise Exception("Invalid shortcode")
+        raise Exception('Invalid shortcode')
 
-
-def change_valid(userid, valid: int, db=DB_PATH):
+def change_valid(userid, valid:int, db=DB_PATH):
     conn = sq.connect(db)
     cur = conn.cursor()
-    if valid in {0, 1}:
-        cur.execute(
-            """
+    if (valid in {0,1}) :
+        cur.execute('''
         UPDATE mapping 
         SET active = ? 
         WHERE user_id = ?
-        """,
-            (valid, str(userid)),
+        ''', (valid,str(userid))
         )
         conn.commit()
     else:
-        raise Exception("Issue changing valid status")
-
+        raise Exception('Issue changing valid status')
 
 def random_quote(author):
-    images = os.listdir("/home/pi/code/icroboticsbot/background_images")
-    backgrounds = [
-        "/home/pi/code/icroboticsbot/background_images/" + image
-        for image in images
-        if image.startswith(author.strip().lower())
-    ]
+    images = os.listdir('/home/pi/code/icroboticsbot/background_images')
+    backgrounds = ['/home/pi/code/icroboticsbot/background_images/'+image for image in images if image.startswith(author.strip().lower())]
     background = random.choice(backgrounds)
-    fonts = os.listdir("/home/pi/code/icroboticsbot/fonts")
-    font = "/home/pi/code/icroboticsbot/fonts/" + random.choice(fonts)
-    with open("/home/pi/code/icroboticsbot/quotes.json", "r") as f:
+    fonts = os.listdir('/home/pi/code/icroboticsbot/fonts')
+    font = '/home/pi/code/icroboticsbot/fonts/'+random.choice(fonts)
+    with open('/home/pi/code/icroboticsbot/quotes.json', 'r') as f:
         quotes = f.readlines()
     quotes = [json.loads(quote) for quote in quotes]
-    choices = [
-        quote for quote in quotes if quote["author"].lower() == author.strip().lower()
-    ]
+    choices = [quote for quote in quotes if quote['author'].lower() == author.strip().lower()]
     choice = random.choice(choices)
-    generate(background, quote=choice["quote"], author=choice["author"], font=font)
-
+    generate(background, quote=choice['quote'], author=choice['author'], font=font)
 
 def download_files(files):
-    ssh = createSSHClient("slicer.local", 22, "member", slicer_secret)
+    ssh = createSSHClient('slicer.local',22,'member', slicer_secret)
     scp = SCPClient(ssh.get_transport())
     for file in files:
-        url = file["url"]
-        name = file["name"]
+        url = file['url']
+        name = file['name']
         r = requests.get(url)
         file = io.BytesIO()
         file.write(r.content)
         file.seek(0)
-        scp.putfo(file, TARGET_PATH + name)
-
+        scp.putfo(file,TARGET_PATH+name)
 
 def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
@@ -174,4 +153,3 @@ def createSSHClient(server, port, user, password):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(server, port, user, password)
     return client
-
