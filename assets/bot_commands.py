fb00fa917562dd2ad59b424d3de5c16315f1bd2b
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-12-11 11:44:29 +0000

    Added docstrings

--- assets/bot_commands.py
+++ assets/bot_commands.py
@@ -8,6 +8,16 @@ DEBUG = False
 
 
 async def register_on_guild(bot, ctx):
+    """
+    register_on_guild Register message when user tries to register on guild
+
+    Parameters
+    ----------
+    bot : DiscordBot
+        Discord bot instance
+    ctx : Discord.Context
+        Discord context
+    """
     embed = discord.Embed(title="How-to register",                                  # noqa
                             description=("To get the membership role."              # noqa
                                         " Please write a message in "               # noqa
@@ -20,25 +30,37 @@ async def register_on_guild(bot, ctx):
 
 
 async def register_on_dm(bot, ctx, shortcode):
+    """
+    register_on_dm Register message when user tries to register on DM
+
+    Parameters
+    ----------
+    bot : DiscordBot
+        Discord bot instance
+    ctx : Discord.Context
+        Discord context
+    shortcode : str
+        Shortcode of the user
+    """
     async def add_role_and_update(server, member, ctx):
         role = discord.utils.get(server.roles, name='ICRS Member')
         await member.add_roles(role, reason="Membership verified using API")
         add_mapping(shortcode, member.id)
-        embed = discord.Embed(title="Verified!",                                                        # noqa
-                              description="You have been verified and should have the ICRS Member role",# noqa
-                              color=0x3a88fe)                                                           # noqa
-        embed.set_footer(text="Go back to the server: https://discord.gg/3YKPjgskS3")                   # noqa
+        embed = discord.Embed(title="Verified!",                                                            # noqa  # pylint: disable
+                              description="You have been verified and should have the ICRS Member role",    # noqa  # pylint: disable
+                              color=0x3a88fe)                                                               # noqa  # pylint: disable
+        embed.set_footer(text="Go back to the server: https://discord.gg/3YKPjgskS3")                       # noqa  # pylint: disable
         await ctx.message.channel.send(embed=embed)
 
     async def update_member_in_db(member, shortcode, ctx):
         if valid_mapping(shortcode, member.id):
-            embed = discord.Embed(title="Error",                                                        # noqa
-                              description="Someone has already verified using this shortcode.",         # noqa
-                              color=0x3a88fe)                                                           # noqa
-            embed.add_field(name="If this is not you:",                                                 # noqa
-                            value="Message a committee member",                                         # noqa
-                            inline=False)                                                               # noqa
-            embed.set_footer(text="You'll find committee members here: https://discord.gg/3YKPjgskS3")  # noqa
+            embed = discord.Embed(title="Error",                                                        # noqa  # pylint: disable
+                              description="Someone has already verified using this shortcode.",         # noqa  # pylint: disable
+                              color=0x3a88fe)                                                           # noqa  # pylint: disable
+            embed.add_field(name="If this is not you:",                                                 # noqa  # pylint: disable
+                            value="Message a committee member",                                         # noqa  # pylint: disable
+                            inline=False)                                                               # noqa  # pylint: disable
+            embed.set_footer(text="You'll find committee members here: https://discord.gg/3YKPjgskS3")  # noqa  # pylint: disable
             await ctx.message.channel.send(embed=embed)
         else:
             change_valid(member.id, 1)
@@ -49,16 +71,16 @@ async def register_on_dm(bot, ctx, shortcode):
             await ctx.message.channel.send(embed=embed)
 
     async def user_not_member(ctx):
-        embed = discord.Embed(title="No membership!",                                               # noqa
-                              description="We couldn't verify your membership.",                    # noqa
-                              color=0x3a88fe)                                                       # noqa
-        embed.add_field(name="To get a membership:",                                                # noqa
-                        value="https://www.imperialcollegeunion.org/activities/a-to-z/robotics",    # noqa
-                        inline=False)                                                               # noqa
-        embed.add_field(name="If you already bought one:",                                          # noqa
-                        value="Please try again later or contact a committee member",               # noqa
-                        inline=False)                                                               # noqa
-        embed.set_footer(text="You'll find committee members here: https://discord.gg/3YKPjgskS3")  # noqa
+        embed = discord.Embed(title="No membership!",                                               # noqa  # pylint: disable
+                              description="We couldn't verify your membership.",                    # noqa  # pylint: disable
+                              color=0x3a88fe)                                                       # noqa  # pylint: disable
+        embed.add_field(name="To get a membership:",                                                # noqa  # pylint: disable
+                        value="https://www.imperialcollegeunion.org/activities/a-to-z/robotics",    # noqa  # pylint: disable
+                        inline=False)                                                               # noqa  # pylint: disable
+        embed.add_field(name="If you already bought one:",                                          # noqa  # pylint: disable
+                        value="Please try again later or contact a committee member",               # noqa  # pylint: disable
+                        inline=False)                                                               # noqa  # pylint: disable
+        embed.set_footer(text="You'll find committee members here: https://discord.gg/3YKPjgskS3")  # noqa  # pylint: disable
         await ctx.message.channel.send(embed=embed)
 
     try:
@@ -73,10 +95,10 @@ async def register_on_dm(bot, ctx, shortcode):
                     else:
                         await update_member_in_db(member, shortcode, ctx)
                 else:
-                    embed = discord.Embed(title="Join the server!",                                             # noqa
-                              description="Looks like you're not on the discord server :(",                     # noqa
-                              url="https://discord.gg/3YKPjgskS3",                                              # noqa
-                              color=0x3a88fe)                                                                   # noqa
+                    embed = discord.Embed(title="Join the server!",                                             # noqa  # pylint: disable
+                              description="Looks like you're not on the discord server :(",                     # noqa  # pylint: disable
+                              url="https://discord.gg/3YKPjgskS3",                                              # noqa  # pylint: disable
+                              color=0x3a88fe)                                                                   # noqa  # pylint: disable
                     await ctx.message.channel.send(embed=embed)
             else:
                 await user_not_member(ctx)
@@ -88,6 +110,18 @@ async def register_on_dm(bot, ctx, shortcode):
 
 
 async def quote_person(bot, ctx, name):  # pylint: disable=unused-argument
+    """
+    quote_person Generate a quote image from the stored quotes
+
+    Parameters
+    ----------
+    bot : DiscordBot
+        Discord bot instance
+    ctx : Discord.Context
+        Discord context
+    name : str
+        Name of the person to quote
+    """
     q, p = random_quote(" ".join(name))
     file = discord.File(p)
     embed = discord.Embed(title=q[0],
@@ -98,6 +132,16 @@ async def quote_person(bot, ctx, name):  # pylint: disable=unused-argument
 
 
 async def get_help(bot, ctx):
+    """
+    get_help Get the help message with all the commands
+
+    Parameters
+    ----------
+    bot : DiscordBot
+        Discord bot instance
+    ctx : Discord.Context
+        Discord context
+    """
     embed = discord.Embed(title="Help",
                           description="List of available commands:")
     for command in bot.commands:
@@ -106,6 +150,16 @@ async def get_help(bot, ctx):
 
 
 async def handle_upload(bot, message):
+    """
+    handle_upload Handle upload of files to the server
+
+    Parameters
+    ----------
+    bot : DiscordBot
+        Discord bot instance
+    message : Discord.Message
+        Discord message
+    """
     files = []
     print("file sent in files")
     for attachment in message.attachments:
