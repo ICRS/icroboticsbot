e5449c3829a154f6989ef78ed3dceac841e78ec4
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-01 22:36:33 +0000

    updated deployment scripts

--- registration/docker_build.sh
+++ registration/docker_build.sh
@@ -1,3 +1,3 @@
-docker build -t localhost:32000/icrsbot-file-handler .
-docker push localhost:32000/icrsbot-file-handler
+docker build -t localhost:32000/icrsbot-registration .
+docker push localhost:32000/icrsbot-registration
 
