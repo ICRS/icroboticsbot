7e92f605d16a5afee2ffea22fad483bb07e108f6
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-01 19:04:32 +0000

    removed redundant variables and optimised imports

--- file_handler/src/utils.py
+++ file_handler/src/utils.py
@@ -8,7 +8,6 @@ Utility functions used by the bot
 """
 
 import os
-from datetime import date
 import io
 import time
 
@@ -16,7 +15,6 @@ import time
 import requests  # type: ignore
 import paramiko  # type: ignore
 
-from icu_ea_api import ICUEActivitiesAPI  # type: ignore
 from scp import SCPClient  # type: ignore
 
 from dotenv import load_dotenv
@@ -28,42 +26,15 @@ __all__ = ["download_files", "create_sshclient",
 
 # ===== Constants =====
 load_dotenv()
-TARGET_PATH = '/home/member/Downloads/'
+# =====================
 
-# ===== Get the current date =====
-date_now = date.today()
-month_now = date_now.month
-year_now = str(date_now.year)
-if month_now > 8:
-    year_string = f"{year_now[2:]}-{int(year_now[2:])+1}"
-else:
-    year_string = f"{int(year_now[2:])-1}-{year_now[2:]}"
-
-# =================================
-
-CSP_CODE = 625
-
-# ===== Get the API key =====
-api_key = os.getenv('API_KEY')
-society_api = ICUEActivitiesAPI(CSP_CODE, api_key, year_string)
-#DB_PATH = os.path.abspath(os.getenv('DB_PATH'))
+# ===== Get Slicer Configurations =====
 SLICER_PW = os.getenv('SLICER_PW')
 SLICER_ADDR = os.getenv('SLICER_ADDR')
+TARGET_PATH = str(os.getenv("TARGET_PATH"))
 # =========================================
 
 extension_list = ['stl', '3mf', 'obj', 'stp', 'step']
-
-
-# ===== Database Schema =====
-INIT_SCHEMA = '''
-CREATE TABLE mapping (
-    user_id  TEXT    PRIMARY KEY,
-    shortcode   TEXT    NOT NULL,
-    active  INTEGER DEFAULT 1
-)
-'''
-SHORTCODE_REGEX = r'[a-z]{2,3}[0-9]{2,4}'
-# ===========================
 
 
 def print(*args, **kwargs) -> None:  # pylint: disable=redefined-builtin
