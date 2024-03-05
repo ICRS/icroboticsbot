f19bb5e98dc987bb8d8b8c11f22e8d08aefb47eb
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-05 02:54:45 +0000

    init stats handler

---
+++ stats_handler/src/bot_class.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# mypy: ignore-errors
+
+"""
+Discord Bot class. It handles all the commands and events.
+"""
+import os
+import json
+import asyncio
+import discord
+from discord.ext import commands
+
+from src.bot_commands import stats_card          # noqa
+
+from src.utils import print                      # noqa  #pylint: disable=redefined-builtin
+from src.utils import BASE_PATH
+__all__ = ["DiscordBot"]
+
+settings = json.load(open(os.path.abspath(BASE_PATH+"settings.json"),
+                          "r", encoding="utf-8"))
+
+PREFIX = settings['PREFIX']
+GUILD = settings['DISCORD_GUILD_ID']
+ADMIN_ID = settings['ADMIN_ID']
+
+default_guild_info = {
+    'PREFIX': PREFIX,
+    'GUILD': GUILD,
+    'ADMIN_ID': ADMIN_ID,
+}
+
+
+class DiscordBot(commands.Bot):
+    # pylint: disable=dangerous-default-value
+    def __init__(self, token, intents,
+                 guild_info=default_guild_info):
+        super().__init__(intents=intents,
+                         command_prefix=guild_info['PREFIX'],
+                         case_insensitive=True,
+                         help_command=None)
+        self.token = token
+        self.guild_info = guild_info
+        self.bot_prefix = guild_info["PREFIX"]
+        self.bot_admin = self.get_user(guild_info["ADMIN_ID"])
+        self.add_commands()
+
+    def add_commands(self):
+        """
+        add_commands adds all the commands to the bot
+        """
+        @self.command(name="stats", pass_context=True,
+                      help="Get your 3D printing stats")
+        async def stats(ctx):
+            await stats_card(self,ctx)
+
+    async def on_message(self, message):  # pylint: disable=arguments-differ
+        """
+        on_message is called when a message is sent in the server
+
+        Parameters
+        ----------
+        message : discord.Message
+            The message sent in the server or DM channel.
+        """
+        if message.author == self.user:
+            return
+
+        await self.process_commands(message)
+
+    async def on_ready(self):
+        """
+        on_ready is called when the bot is ready to be used
+        """
+        guild = discord.utils.get(self.guilds, id=self.guild_info['GUILD'])
+        print(f'Connected to {guild.name}, id: {guild.id}')
+
+    def start_loop(self):
+        """
+        start_loop starts the bot and the alert background task
+        """
+        async def run_bot():
+            await self.start(self.token)
+            await self.close()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(run_bot())
