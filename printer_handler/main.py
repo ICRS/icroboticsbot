fc43253008ed405fb067702f5252df7e71fb777a
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2024-03-29 02:55:43 +0000

    Linting

--- printer_handler/main.py
+++ printer_handler/main.py
@@ -1,11 +1,10 @@
+import json
+import os
+import signal
+import time
+
 from src.bot_class import PrinterWebhook
 
-import signal 
-
-import os
-import json 
-
-import time
 
 WEBHOOK_URL = str(os.getenv("WEBHOOK_URL")).strip()
 settings = json.load(open("settings.json", "r", encoding="utf-8"))
@@ -13,12 +12,13 @@ PRINTER_NAMES = list(settings["PRINTER_NAMES"])
 PRINTER_GATEWAY_ENDPOINT_SUFFIX = settings["PRINTER_GATEWAY_ENDPOINT_SUFFIX"]
 
 if __name__ == '__main__':
-    w = PrinterWebhook(WEBHOOK_URL, PRINTER_NAMES, PRINTER_GATEWAY_ENDPOINT_SUFFIX)
-    
+    w = PrinterWebhook(WEBHOOK_URL, PRINTER_NAMES,
+                       PRINTER_GATEWAY_ENDPOINT_SUFFIX)
+
     signal.signal(signal.SIGINT, w.delete_message)
     signal.signal(signal.SIGTERM, w.delete_message)
 
     while True:
         w.send_message_all()
         time.sleep(15)
-    
+
