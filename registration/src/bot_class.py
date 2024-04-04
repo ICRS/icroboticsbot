475789bd713875955ca422c4f32bb555d16b850f
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-04-04 12:04:16 +0100

    removed redundant discord admin vars

--- registration/src/bot_class.py
+++ registration/src/bot_class.py
@@ -45,7 +45,6 @@ class DiscordBot(commands.Bot):
         self.token = token
         self.guild_info = guild_info
         self.bot_prefix = guild_info["PREFIX"]
-        self.bot_admin = self.get_user(guild_info["ADMIN_ID"])
         self.add_commands()
 
     def add_commands(self):
