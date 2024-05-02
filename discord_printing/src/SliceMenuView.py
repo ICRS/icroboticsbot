5255c24718b5bd927d94a22848a83d5d592fbaca
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2024-05-03 00:16:09 +0100

    Fixed circular imports and saved options

--- discord_printing/src/SliceMenuView.py
+++ discord_printing/src/SliceMenuView.py
@@ -1,20 +1,84 @@
+import logging
+
 import discord
 from discord.ui import View, Button
-
-from src.LayerHeightOptions import LayerHeightMenu
-from src.InfillOptions import InfillMenu
 
 
 __all__ = ["SliceMenuGeneral"]  # noqa
 
 slice_options = {
+    "filename": "",
     "height": 0.28,
     "infill": 15,
 }
 
 
 def send_slice_to_gateway():
-    print("Sending slice to gateway")
+    logging.info("Sending slice to gateway")
+
+
+infill_options = [5, 10, 15, 20, 25, 30]
+layer_options = [0.08, 0.12, 0.16, 0.20, 0.24, 0.28]
+
+
+class LayerButton(Button):
+    def __init__(self, height=layer_options[-1], **kwargs):
+        super().__init__(**kwargs)
+        self.height = height
+
+    async def callback(self, interaction: discord.Interaction):
+        """
+        callback is called when the button is clicked
+
+        Parameters
+        ----------
+        interaction : discord.Interaction
+            Discord interaction
+        """
+        self.disabled = True  # Disable the button after being clicked
+        await interaction.response.edit_message(
+            embed=None,
+            view=SliceMenuGeneral(height=self.height),
+            delete_after=60)
+
+
+class LayerHeightMenu(View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+        for layer in layer_options:
+            self.add_item(LayerButton(height=str(layer),
+                                      label=str(layer)+"mm",
+                                      style=discord.ButtonStyle.green))
+
+
+class InfillButton(Button):
+    def __init__(self, infill=infill_options[2], **kwargs):
+        super().__init__(**kwargs)
+        self.infill = infill
+
+    async def callback(self, interaction: discord.Interaction):
+        """
+        callback is called when the button is clicked
+
+        Parameters
+        ----------
+        interaction : discord.Interaction
+            Discord interaction
+        """
+        self.disabled = True  # Disable the button after being clicked
+        await interaction.response.edit_message(
+            embed=None,
+            view=SliceMenuGeneral(infill=self.infill),
+            delete_after=60)
+
+
+class InfillMenu(View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+        for infill in infill_options:
+            self.add_item(InfillButton(infill=str(infill),
+                                       label=str(infill)+"%",
+                                       style=discord.ButtonStyle.green))
 
 
 class SliceMenuGeneral(View):
@@ -46,9 +110,12 @@ class SliceMenuGeneral(View):
     async def confirm(self, interaction: discord.Interaction,
                       button: discord.ui.Button):
         button.style = discord.ButtonStyle.gray
+        embed_message = discord.Embed(title=f"{slice_options['filename']}",
+                                      description=f"Slice options:\nLayer Height: {slice_options['height']}mm\nInfill: {slice_options['infill']}%", # noqa
+                                      color=discord.Color.green())
         send_slice_to_gateway()
         await interaction.response.edit_message(
-            content="Options selected. Confirming...",
+            content=None,
             view=None,
-            embed=None,
-            delete_after=60)
+            embed=embed_message,
+            delete_after=30)
