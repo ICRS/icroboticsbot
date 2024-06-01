b4c638b44cfbbb05f2622f21945a7f8dde217359
icrs <icrs@icrs>
2024-06-01 21:33:06 +0100

    added social links

--- registration/src/bot_messages.py
+++ registration/src/bot_messages.py
@@ -1,23 +1,28 @@
 import logging
 import discord
 
+error_color = 0xfa4646
+info_color = 0x297bff
+success_color = 0x46fa64
+
+
 def success_msg():
         embed = discord.Embed(title="Verified!",                                                            # noqa  # pylint: disable
                               description="You have been verified and should have the ICRS Member role",    # noqa  # pylint: disable
-                              color=0x3a88fe)                                                               # noqa  # pylint: disable
-        embed.set_footer(text="Go back to the server: https://discord.gg/3YKPjgskS3")                       # noqa  # pylint: disable
+                              color=success_color)                                                               # noqa  # pylint: disable
+        embed.set_footer(text="Check out our Insta too: https://linktr.ee/icrobotics")                       # noqa  # pylint: disable
         return embed
 
 def reverified_msg():
     embed = discord.Embed(title="Membership reverified",                            # noqa
                               description="Welcome back!",                                  # noqa
-                              color=0x3a88fe)                                               # noqa
-    return embed.set_footer(text="Go back to the server: https://discord.gg/3YKPjgskS3")   # noqa
+                              color=success_color)                                               # noqa
+    return embed.set_footer(text="Check out our Insta too: https://linktr.ee/icrobotics")   # noqa
 
 def code_already_used_msg():
     embed = discord.Embed(title="Error",                                                        # noqa  # pylint: disable
                       description="Someone has already verified using this shortcode.",         # noqa  # pylint: disable
-                      color=0x3a88fe)                                                           # noqa  # pylint: disable
+                      color=error_color)                                                           # noqa  # pylint: disable
     embed.add_field(name="If this is not you:",                                                 # noqa  # pylint: disable
                     value="Message a committee member",                                         # noqa  # pylint: disable
                     inline=False)                                                               # noqa  # pylint: disable
@@ -26,9 +31,9 @@ def code_already_used_msg():
 def user_not_member_msg():
         embed = discord.Embed(title="No membership!",                                               # noqa  # pylint: disable
                               description="We couldn't verify your membership.",                    # noqa  # pylint: disable
-                              color=0x3a88fe)                                                       # noqa  # pylint: disable
+                              color=error_color)                                                       # noqa  # pylint: disable
         embed.add_field(name="To get a membership:",                                                # noqa  # pylint: disable
-                        value="https://www.imperialcollegeunion.org/activities/a-to-z/robotics",    # noqa  # pylint: disable
+                        value="https://linktr.ee/icrobotics",                                       # noqa  # pylint: disable
                         inline=False)                                                               # noqa  # pylint: disable
         embed.add_field(name="If you already bought one:",                                          # noqa  # pylint: disable
                         value="Please try again later or contact a committee member",               # noqa  # pylint: disable
@@ -39,15 +44,17 @@ def not_on_guild_msg():
     return discord.Embed(title="Join the server!",                                             # noqa  # pylint: disable
           description="Looks like you're not on the discord server :(",                     # noqa  # pylint: disable
           url="https://discord.gg/3YKPjgskS3",                                              # noqa  # pylint: disable
-          color=0x3a88fe)       
+          color=info_color)       
 
 def how_to_msg():
-    return discord.Embed(title="How-to register",                                  # noqa
+    embed = discord.Embed(title="How-to register",                                  # noqa
                        description=("To get the membership role."              # noqa
                                    " Please reply to THIS message message in "               # noqa
                                    f"format:\n```!register yourShortcodeHere``` \n"          # noqa
                                    f"Example:\n ```!register dc1021```"),                              # noqa
-                               color=0xFF5733)                                 # noqa
+                               color=info_color)                                 # noqa
+    return embed.set_footer(text="Check out our Insta too: https://linktr.ee/icrobotics")                       # noqa  # pylint: disable
+
 
 async def error_msg(ctx, e):
     logging.error(f"Error in registering user: {e}")
