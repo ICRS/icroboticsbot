93c0396e8c86dbefe23b536ed916b2e46e60e7e0
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-06-25 16:29:07 +0100

    add health check for printer handler

--- printer_handler/main.py
+++ printer_handler/main.py
@@ -2,15 +2,14 @@ import json
 import logging
 import os
 import signal
+import threading
 import time
+import uvicorn
 
 from src.bot_class import PrinterWebhook
+from fastapi import FastAPI, Response, status
 
-
-WEBHOOK_URL = str(os.getenv("WEBHOOK_URL")).strip()
-settings = json.load(open("settings.json", "r", encoding="utf-8"))
-PRINTER_NAMES = list(settings["PRINTER_NAMES"])
-PRINTER_GATEWAY_ENDPOINT_SUFFIX = settings["PRINTER_GATEWAY_ENDPOINT_SUFFIX"]
+app = FastAPI()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,13 +19,36 @@ logging.basicConfig(
         logging.StreamHandler()
     ])
 
+
+WEBHOOK_URL = str(os.getenv("WEBHOOK_URL")).strip()
+settings = json.load(open("settings.json", "r", encoding="utf-8"))
+PRINTER_NAMES = list(settings["PRINTER_NAMES"])
+PRINTER_GATEWAY_ENDPOINT_SUFFIX = settings["PRINTER_GATEWAY_ENDPOINT_SUFFIX"]
+
+
+w = PrinterWebhook(WEBHOOK_URL, PRINTER_NAMES,
+                   PRINTER_GATEWAY_ENDPOINT_SUFFIX)
+
+signal.signal(signal.SIGINT, w.delete_message)
+signal.signal(signal.SIGTERM, w.delete_message)
+
+
+@app.get("/healthz", status_code=200)
+def health_check(response: Response):
+    healthy = w.health_check()
+    if not healthy:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    return healthy
+
+
+class BackgroundTasks(threading.Thread):
+    def run(self, *args, **kwargs):
+        while True:
+            w.send_message_all()
+            time.sleep(10)
+
+
 if __name__ == '__main__':
-    w = PrinterWebhook(WEBHOOK_URL, PRINTER_NAMES,
-                       PRINTER_GATEWAY_ENDPOINT_SUFFIX)
-
-    signal.signal(signal.SIGINT, w.delete_message)
-    signal.signal(signal.SIGTERM, w.delete_message)
-
-    while True:
-        w.send_message_all()
-        time.sleep(10)
+    t = BackgroundTasks()
+    t.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
