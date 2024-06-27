5adc36830269c43dd66aa39cd9221ff1ce4af01a
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-06-27 15:24:25 +0100

    fix formatting

--- stats_handler/src/bot_commands.py
+++ stats_handler/src/bot_commands.py
@@ -16,6 +16,7 @@ DEBUG = False
 
 __all__ = ["stats_card"]  # noqa
 
+
 async def stats_card(ctx):
     """
     stats_card generates a card with 3d printer usage stats for that user
@@ -31,10 +32,10 @@ async def stats_card(ctx):
         card = generate_stat_card(user)
     except Exception as e:
         logging.error(f"Could not generate stats {e}")
-    
+
     with io.BytesIO() as image_binary:
         card.save(image_binary, 'PNG')
         image_binary.seek(0)
         file = discord.File(image_binary, filename="image.png")
         embed.set_image(url=f"attachment://{file.filename}")
-        await ctx.send(file=file,embed=embed)
+        await ctx.send(file=file, embed=embed)
