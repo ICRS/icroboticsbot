0e27bbed2030c98a6640597565cac61f735b6211
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-06 03:59:35 +0100

    migrate registration stuff to backend

--- registration/src/utils/validation.py
+++ registration/src/utils/validation.py
@@ -2,18 +2,21 @@ import re
 
 
 SHORTCODE_REGEX = r'^[a-z]{2,3}[0-9]{2,4}$'
-UID_REGEX= r'^[0-9A-F]{8,14}$'
+UID_REGEX = r'^[0-9A-F]{8,14}$'
 DISCORD_ID_REGEX = r'^<@[0-9]{18}>$'
+
 
 def is_shortcode(message: str) -> bool:
     message = message.lower().strip()
     found = re.findall(SHORTCODE_REGEX, message)
     return any(found)
 
+
 def is_uid(message: str) -> bool:
     message = format_uid(message)
     found = re.findall(UID_REGEX, message)
     return any(found)
+
 
 def format_uid(message: str) -> bool:
     message = message.upper()
@@ -21,6 +24,7 @@ def format_uid(message: str) -> bool:
     message = message.replace(":", "")
     message = message.replace("-", "")
     return message
+
 
 def is_discord_id(id: str) -> bool:
     found = re.findall(DISCORD_ID_REGEX, id)
