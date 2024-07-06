a83ca1b648ab15477328845fb5824ff990ac0cd5
icrs <icrs@icrs>
2024-07-06 18:42:20 +0100

    bring over meme bot

---
+++ registration/src/commands/quote.py
@@ -0,0 +1,97 @@
+import logging
+import discord
+import os
+import random
+import json
+from typing import Dict, List
+import io
+from PIL import Image
+
+from src.utils.error_msg import *
+from src.utils.quote_utils import generate
+
+async def quote_person(interaction, name):  # pylint: disable=unused-argument
+    """
+    quote_person Generate a quote image from the stored quotes
+
+    Parameters
+    ----------
+    interaction : Discord.interaction
+        interaction
+    name : str
+        Name of the person to quote
+    """
+    logging.info(f"{interaction.user} requested a quote")
+    temp = io.BytesIO()
+    q, img = await random_quote(interaction, name)
+
+    if(q == None):
+        return
+
+    img.save(temp, format="PNG")
+    temp.seek(0)
+    
+    file = discord.File(temp, filename="quote.png")
+
+    embed = discord.Embed(title=q[0],
+                          description=q[1],
+                          color=0x3a88fe)
+
+    embed.set_image(url=f"attachment://{file.filename}.png")
+
+    await interaction.response.send_message(embed=embed, file=file)
+
+
+async def random_quote(interaction, author: str) -> tuple[str, Image.Image]:
+    """
+    random_quote generates a random quote image for a given author
+
+    Parameters
+    ----------
+    author : str
+        Author of the quote
+
+    Returns
+    -------
+    tuple
+        A tuple containing the quote and the PIL Image object
+    """
+    image_list = os.listdir(os.path.relpath('assets/background_images'))
+    logging.info(f"Images: {image_list}")
+    logging.info(f"Author: {author}")
+
+    author = author.replace(" ", "").lower()
+
+    backgrounds = [os.path.relpath('assets/background_images/'+image)
+                   for image in image_list if image.startswith(
+                       author)]
+    if(len(backgrounds) == 0):
+        await interaction.response.send_message(embed=quote_not_found())
+        return None, None
+
+
+    logging.info(f"Backgrounds: {backgrounds}")
+    background = random.choice(backgrounds)
+    fonts = os.listdir(os.path.relpath('assets/fonts'))
+    font = os.path.relpath('assets/fonts/'+random.choice(fonts))
+    logging.info(f"Background: {background} Font: {font}")
+    with open(os.path.relpath('assets/quotes.json'), 'r',
+              encoding="utf-8") as f:
+        quotes = f.readlines()
+    quotes = [json.loads(quote) for quote in quotes]
+    author = author.strip().lower()
+    choices: Dict[str, List[str]] = {
+        quote['author'].lower(): [] for quote in quotes}
+    if not author:
+        author = random.choice(list(choices.keys()))
+    for quote in quotes:
+        choices[quote['author'].lower()].append(quote['quote'])     # noqa
+    choice = random.choice(choices[author])
+    logging.info(f"Quote: {choice} Author: {author} Background: {background} Font: {font}")
+    
+    img = generate(background, quote=choice,              # noqa  # pylint: disable=unused-variable
+                             author=author.capitalize(), font=font)
+
+    return (author.capitalize(), choice), img
+
+
---
+++ registration/src/utils/quote_utils.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import io
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+def generate(IMAGE_PATH, author, quote,
+             font=("assets/fonts/Precious.ttf")) -> Image.Image: # noqa
+    """
+    generate a quote image from a given image and quote
+
+    Parameters
+    ----------
+    IMAGE_PATH : String
+        Path to the Image
+    author : String
+        Author of quote
+    quote : String
+        Quote
+    font : String, optional
+        Path to the font file, by default "assets/fonts/Precious.ttf"
+
+    Returns
+    -------
+    PIL.Image.Image
+        PIL Image Object
+    """  # noqa ignore
+    
+    logging.info(f"Generating Quote Image from {IMAGE_PATH}")
+    image = Image.open(os.path.relpath(IMAGE_PATH))
+    logging.info(f"Image Opened")
+    grayscale = image.convert("L")
+    width, height = grayscale.size
+    ratio = 400/width
+    grayscale = grayscale.resize((int(width*ratio), int(height*ratio)))
+    logging.info(f"Image Resized")
+    temp = io.BytesIO()
+
+    grayscale.save(temp, format="PNG")
+
+    logging.info(f"Image Temp Saved")
+
+    img = convert(
+            quote=quote,
+            author=author,
+            fg="white",
+            image=temp,
+            border_color="black",
+            font_size=40,
+            font_file=font,
+            width=400,
+            height=400)
+
+    return img
+
+
+def convert(quote, author, fg, image: Image.Image, border_color,
+            font_file=None, font_size=None, width=None, height=None):
+    """
+    convert a quote to an image
+
+    Parameters
+    ----------
+    quote : String
+        Quote to be converted
+    author : String
+        Author of the Quote
+    fg : String
+        Foreground Color
+    image : PIL.Image.Image
+        Image to be used as background
+    border_color : String
+        Border Color
+    font_file : String, optional
+        Path to font file, by default None
+    font_size : int, optional
+        Font size, by default None
+    width : int, optional
+        Width of new image, by default None
+    height : int, optional
+        Height of new image, by default None
+
+    Returns
+    -------
+    PIL.Image.Image
+        Image with quote
+    """
+    x1 = width if width else 612
+    y1 = height if height else 612
+
+    sentence = f"{quote} - {author}"
+
+    font = ImageFont.truetype(font_file if font_file
+                              else os.path.relpath("assets/fonts/Coves Bold.otf"), font_size  # noqa # pylint: disable=line-too-long
+                              if font_size else 32)
+
+    img = Image.new("RGB", (x1, y1), color=(255, 255, 255))
+
+    back = Image.open(image, 'r')
+    img_w, img_h = back.size
+    bg_w, bg_h = img.size
+    offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+    bback = back.filter(ImageFilter.BLUR)
+    img.paste(bback, offset)
+
+    d = ImageDraw.Draw(img)
+
+    sum_value = 0
+    for letter in sentence:
+        s = font.getbbox(letter)
+        sum_value += s[2] - s[0]
+    average_length_of_letter = sum_value / len(sentence)
+
+    number_of_letters_for_each_line = (x1 / 1.618) / average_length_of_letter
+    incrementer = 0
+    fresh_sentence = ""
+
+    for letter in sentence:
+        if letter == "-":
+            fresh_sentence += "\n\n" + letter
+        elif incrementer < number_of_letters_for_each_line:
+            fresh_sentence += letter
+        else:
+            if letter == " ":
+                fresh_sentence += "\n"
+                incrementer = 0
+            else:
+                fresh_sentence += letter
+        incrementer += 1
+
+    x2 = max([font.getbbox(line) for line in fresh_sentence.split('\n')],
+             key=lambda x: x[2] - x[0])[2]
+    fline = font.getbbox(fresh_sentence.split('\n')[0])
+    y2 = (fline[3] - fline[1]) * len(fresh_sentence.split('\n'))
+
+    qx = x1 / 2 - x2 / 2
+    qy = y1 / 2 - y2 / 2
+
+    d.text((qx-1, qy-1), fresh_sentence, align="center",
+           font=font, fill=border_color)
+    d.text((qx+1, qy-1), fresh_sentence, align="center",
+           font=font, fill=border_color)
+    d.text((qx-1, qy+1), fresh_sentence, align="center",
+           font=font, fill=border_color)
+    d.text((qx+1, qy+1), fresh_sentence, align="center",
+           font=font, fill=border_color)
+
+    d.text((qx, qy), fresh_sentence, align="center", font=font, fill=fg)
+
+    return img
