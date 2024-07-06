9d2b4959284f15d9fc6f00da5395b79cf3d5ee21
icrs <icrs@icrs>
2024-07-06 18:41:02 +0100

    refactor messages

---
+++ registration/src/utils/info_msg.py
@@ -0,0 +1,23 @@
+import discord
+
+info_color = 0x297bff
+
+
+def not_on_guild_msg():
+    return discord.Embed(
+        title="Join the server!",
+        description="Looks like you're not on the discord server :(",
+        url="https://discord.gg/3YKPjgskS3",
+        color=info_color)
+
+
+def how_to_msg():
+    embed = discord.Embed(
+        title="How-to register",
+        description=("To get the membership role."
+                     " Please reply to THIS message like this "
+                     "format:\n```!register yourShortcodeHere``` \n"
+                     "Example:\n ```!register dc1021```"),
+        color=info_color)
+    return embed.set_footer(
+        text="Check out our Insta too: https://linktr.ee/icrobotics")
