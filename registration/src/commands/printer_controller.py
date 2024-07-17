8e1741db2aa5b4fac548a3fbc9b9afe2c03b11aa
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-17 16:38:05 +0100

    update printer controller to get frame

--- registration/src/commands/printer_controller.py
+++ registration/src/commands/printer_controller.py
@@ -1,9 +1,13 @@
 __all__ = ["printer_controller"]
 
 
+import asyncio
+import base64
+from io import BytesIO
+from PIL import Image
 from typing import Any
 import aiohttp
-from discord import ButtonStyle, Interaction, Embed, Color, Message, User
+from discord import ButtonStyle, File, Interaction, Embed, Color, Message, User
 from discord.ui import View, Button
 
 
@@ -29,6 +33,56 @@ async def printer_controller(user: User,
 
 
 class PrinterController:
+    def __init__(
+            self,
+            printer_name: str,
+            printer_suffix: str,
+            discord_user: User,
+            timeout: int = 10
+    ) -> None:
+        self.printer_name = printer_name
+        self.printer_suffix = printer_suffix
+
+        self.timeout = timeout
+        self.user = discord_user
+
+        self.message = None
+
+        self.printer_controller_interface = PrinterControllerInterface(
+            printer_name=printer_name,
+            printer_suffix=printer_suffix
+        )
+
+    async def printer_update(self):
+        image = self.printer_controller_interface.get_image()
+
+        with BytesIO() as image_binary:
+            image.save(image_binary, 'PNG')
+            image_binary.seek(0)
+            image_bytes = image_binary.getbuffer().tobytes()
+
+        if not self.message:
+            embed = Embed(
+                title=f"Printer {self.printer_name}",
+                description="Print Viewer and controller",
+                color=Color.blurple(),
+            )
+
+            image_file = File(image_bytes, "attachments://image.png")
+
+            self.message: Message = self.user.send(
+                embed=embed,
+                view=PrinterControllerMainPage(
+                    self.printer_controller_interface
+                ),
+                file=image_file
+            )
+        else:
+            self.message.edit()
+        await asyncio.sleep(self.timeout)
+
+
+class PrinterControllerInterface:
     def __init__(self, printer_name: str, printer_suffix: str) -> None:
         self.printer_name = printer_name
         self.printer_url = f"http://{printer_name}{printer_suffix}"
@@ -50,17 +104,33 @@ class PrinterController:
 
         return status_code == 200
 
+    async def get_image(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                    f"{self.printer_url}/printer/camera") as response:
+                status_code = response.status
+                data = await response.json()
+        if status_code != 200:
+            return None
+        else:
+            frame = data['frame'].get(
+                "body", None) if 'frame' in data else None
+
+            if frame is None:
+                return None
+
+            frame = Image.open(BytesIO(base64.b64decode(frame)))
+            return frame
+
 
 class PrinterControllerMainPage(View):
     def __init__(
         self,
-        print_name: str,
-        printer_suffix: str,
+        printer_controller: PrinterControllerInterface
     ):
         super().__init__()
-        self.printer_controller = PrinterController(
-            printer_name=print_name,
-            printer_suffix=printer_suffix)
+
+        self.printer_controller = printer_controller
 
         self.add_item(
             PrinterControlButton(
