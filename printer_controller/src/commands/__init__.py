6f80d3737b140a0873edde0c8489145945c5e008
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-08-13 11:40:41 +0100

    split printer controller to separate services

---
+++ printer_controller/src/commands/__init__.py
@@ -0,0 +1,4 @@
+from .printer_controller import *
+
+__all__ = []
+__all__.extend(printer_controller.__all__)
---
+++ printer_controller/src/utils/api.py
@@ -0,0 +1,79 @@
+__all__ = [
+    "SERVER_IP",
+    "get_current_user_printer",
+    "get_state",
+]
+
+"""
+Utility functions used by the bot
+"""
+
+import logging
+import os
+
+import aiohttp
+import requests
+from requests.auth import HTTPBasicAuth
+from bambulabs_api import GcodeState
+
+
+SERVER_IP = os.getenv("SERVER_IP")
+DATABASE_ADAPTER_USER = os.getenv("DATABASE_ADAPTER_USER")
+DATABASE_ADAPTER_PASSWORD = os.getenv("DATABASE_ADAPTER_PASSWORD")
+
+BASIC_AUTH = HTTPBasicAuth(DATABASE_ADAPTER_USER, DATABASE_ADAPTER_PASSWORD)
+
+
+async def get_current_user_printer(printer_name: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f"{SERVER_IP}/print-metrics/current/printer/shortcode",
+                params={
+                    "printer_name": printer_name
+                }
+        ) as response:
+            status_code = response.status
+            if status_code == 204:
+                return None
+
+            data: dict = await response.json()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f"{SERVER_IP}/shortcode/discord-id",
+                params={
+                    "shortcode": data
+                }
+        ) as response:
+            status_code = response.status
+            if status_code == 204:
+                return None
+
+            data: dict = await response.json()
+
+    return int(data.get("discord_id"))
+
+
+def get_state(printer_url) -> GcodeState:
+    """
+    Retrieves the state of the printer.
+
+    Returns
+    -------
+    State: The state of the printer.
+    """
+    response: requests.Response = requests.Response()
+    try:
+        response = requests.get(
+            f"{printer_url}/printer/status/state",
+            timeout=5)
+    except Exception as e:
+        logging.error(f"{printer_url} Error getting state: {e}")
+    if response.status_code != 200:
+        return GcodeState.UNKNOWN
+    r: dict = response.json()
+    return GcodeState(r.get("state", "IDLE"))
+
+
+if __name__ == '__main__':
+    pass
