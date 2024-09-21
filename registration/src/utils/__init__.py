65a8860c0e8937a71fee824b53f1a3b6d73f224e
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-09-21 22:46:42 +0100

    remore redundant quote utils

--- registration/src/utils/__init__.py
+++ registration/src/utils/__init__.py
@@ -1,6 +1,5 @@
 from .messages import *
 from .api import *
-from .quote_utils import *  
 from .validation import *  
 from .printer import *  
 
@@ -8,6 +7,5 @@ from .printer import *
 __all__ = []
 __all__.extend(messages.__all__)
 __all__.extend(api.__all__)
-__all__.extend(quote_utils.__all__)
 __all__.extend(validation.__all__)
 __all__.extend(printer.__all__)
