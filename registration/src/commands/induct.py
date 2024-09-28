3c7072c50e9b5159c799829175cd56192fc953ee
Etienne Naude <eti@naude.dev>
2024-09-28 18:47:09 +0100

    fix induct command

--- registration/src/commands/induct.py
+++ registration/src/commands/induct.py
@@ -4,19 +4,15 @@ __all__ = [
 
 import logging
 import discord
-import requests
 
 import src.utils as util_msg
-
-from .stats import SERVER_IP
-from .quiz import addRoletoUser
-
+from src.utils.induction_utils import fullInduction
 
 async def induct_member(
         interaction: discord.Interaction,
         shortcode: str,
-        member: discord.User):
-    logging.info(f"User: {interaction}")
+        member: discord.Member):
+    logging.info(f"trying to manually induct: {member.name}")
 
     author = interaction.user
     roles = [r for r in author.roles if r is not None]
@@ -32,42 +28,4 @@ async def induct_member(
         ephemeral=True
     )
 
-    reworked = await addRoletoUser(interaction, shortcode, member)
-    result = requests.post(
-        SERVER_IP + "/induction/induct/discord-id",
-        params={"id": str(member.id)})
-
-    if result.status_code == 200 and reworked:
-        return await interaction.response.edit_message(
-            embed=discord.Embed(
-                "Successfully inducted user",
-                color=discord.Color.green()
-            )
-        )
-    elif result.status_code != 200:
-        logging.warning(f"Induct Member Partially Failed: {member} - "
-                        f"{shortcode}; {result.status_code}, {result.reason}")
-        return await interaction.response.edit_message(
-            embed=discord.Embed(
-                "Could not update db for some reason",
-                color=discord.Color.red()
-            )
-        )
-    elif not reworked:
-        logging.warning(f"Induct Member Partially Failed: {member} - "
-                        f"{shortcode}; Role update failed!")
-        return await interaction.response.edit_message(
-            embed=discord.Embed(
-                "Did not rework user discord permissions...",
-                color=discord.Color.red()
-            )
-        )
-    else:
-        logging.warning(f"Induct Member Severe Failure: {member} - "
-                        f"{shortcode}; {result.status_code}, {result.reason}")
-        return await interaction.response.edit_message(
-            embed=discord.Embed(
-                "Something really bad happened, check the logs.",
-                color=discord.Color.red()
-            )
-        )
+    await fullInduction(interaction, shortcode, member)
