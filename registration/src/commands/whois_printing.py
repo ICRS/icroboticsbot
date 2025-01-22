1259649cf8ea140bf7896ed7796a7c90c45b738b
Will <will.crow@sky.com>
2025-01-22 16:02:29 +0000

    final changes

--- registration/src/commands/whois_printing.py
+++ registration/src/commands/whois_printing.py
@@ -4,34 +4,37 @@ __all__ = [
 
 import discord
 import logging
-
-import discord.ext
 import src.utils as utils
 
 @utils.committee_command
 async def who_is_printing(interaction: discord.Interaction):
-    
-    printer_names = ["Printy Mcprintface", 
-                 "Printer Cheung", 
-                 "Freddy Printer",
-                 "Eric Printman", 
-                 "Andrew Printerson", 
-                 "Additive Spiers"]
+    printer_names = [
+        "Printy Mcprintface", 
+        "Printer Cheung", 
+        "Freddy Printer",
+        "Eric Printman", 
+        "Andrew Printerson", 
+        "Additive Spiers"
+    ]
     
     all_printer_info = ""
-    
+    embeds = []
+
     for printer in printer_names:
-        
         try:
-        
             user = await utils.get_current_user_printer(printer)
             shortcode = utils.get_shortcode_from_discord(user)
             
-            all_printer_info += f'{printer} is currently being used by: \n ID: <@{user}> \n Shortcode: {shortcode}\n\n'
-            
+            if user is None:
+                all_printer_info += f'**{printer}** is currently not in use \n'
+            else:
+                all_printer_info += f'**{printer}** is currently being used by: \nID: <@{user}> \nShortcode: {shortcode}\n\n'
+        
         except Exception as e:
-            
-            all_printer_info += utils.error_msg(printer, "Bad Response")
-            
+            error_embed = utils.error_msg(printer, "Bad Response")
+            embeds.append(error_embed)
+    
     await interaction.response.send_message(content=all_printer_info, ephemeral=True)
-       
+
+    for embed in embeds:
+        await interaction.followup.send(embed=embed, ephemeral=True)
